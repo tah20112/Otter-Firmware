6993e5a7c85f650b9a7fa39069f9a202169335b6
Kelly Brennan <kelly.brennan@students.olin.edu>
2017-08-03 16:00:31 -0400

    Added temperature sensor (DS18B20) measurements

--- IntegratedInterface_Test1/IntegratedInterface_Test1.ino
+++ IntegratedInterface_Test1/IntegratedInterface_Test1.ino
@@ -1,9 +1,14 @@
 
 
-//Libraries
+//7-Seg. Libraries
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
+
+//Temperature probe libraries - Using DS18B20 thermometer
+#include <DallasTemperature.h> // Temperature probe processing library
+#include <OneWire.h> // Temperature probe reading library
+#include <math.h> // Arduino library for # processing
 
 //Button Variables
 int PWR_BUTTON_PIN = A3; 
@@ -30,6 +35,12 @@ int downTemp = 0;
 int minTemp = 28; 
 int maxTemp = 38;
 
+//Temperature reading setup
+#define ONE_WIRE_BUS 7
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire); 
+
+float currentTemp; //temperature measurement
 
 void setup() {
   // put your setup code here, to run once:
@@ -45,38 +56,35 @@ void setup() {
   #endif
   matrix.begin(0x70); // begins I2C communication with seven segment display
   matrix.setBrightness(3); // sets brightness, on scale of 0 (dim) to 15 (bright)
+  turnOffDisp();
+
+  //Temperature probe
+  sensors.begin(); 
 }
 
 void loop() {
 // Power button code
-int power_current = analogRead(PWR_BUTTON_PIN); // read power button state
+  int power_current = analogRead(PWR_BUTTON_PIN); // read power button state
   if (power_current >= 1000 && power_last_state <= 50) {
-    if (!sevseg_on){
-      refreshDisplay(); 
-      sevseg_on = true;
-    }
-    else{
-      for (int i=0 ; i < 5 ; i++) {
-        matrix.writeDigitRaw( i, 0 );
-      }
-      matrix.writeDisplay(); 
+    if (sevseg_on){
+      turnOffDisp(); 
       sevseg_on = false;
     }
+    else{
+      currentTempUpdate(); 
+      sevseg_on = true;
+    }
   }
-
+  if (sevseg_on) {
+     currentTempUpdate();  
+  }
   power_last_state = power_current; // refresh button state in memory
-//  Serial.print("Power_Current: "); 
-//  Serial.println(power_current); 
-//  Serial.print("7-Seg. Vcc: "); 
-//  serial.print(); 
-  
+
 // Alarm button code
   int alarm_current = analogRead(ALARM_BUTTON_PIN);  // read alarm button state
   if (alarm_current >= 1000 && alarm_last_state <= 50) { //if alarm button is pressed anew, min threshold 50 for debouncing (despite pull down) 
     digitalWrite(HOT_LED, !digitalRead(HOT_LED));       //toggled the LED state
   }
-//  Serial.print("Alarm_current: "); 
-//  Serial.println(alarm_current);
   alarm_last_state = alarm_current; // refresh button state in memory
   
 // 7-Seg. Display Code
@@ -85,28 +93,51 @@ int power_current = analogRead(PWR_BUTTON_PIN); // read power button state
   if (upTemp >= 1000 && setTemp < maxTemp) {  // condition that membrane switch is high (pressed) and that the desired set temp is less than max
 //    insert debouncing code here if necessary
     setTemp = setTemp+1; // increment set temperature by 1
-    refreshDisplay(); 
+    setTempUpdate();
     delay(400); // debouncing
   }
   if (downTemp >= 1000 && setTemp > minTemp){ //condition that membrane switch is high (pressed) and that desired set temp is greater than min
     setTemp = setTemp-1; // increment set temperature by -1
-    refreshDisplay(); 
+    setTempUpdate(); 
     delay (400); // debouncing
   }
 //  Serial.print("Analog Read: "); 
 //  Serial.print(upTemp); 
 //  Serial.print(" Set Temp: "); 
 //  Serial.println(setTemp);   
-  
 }
 
-void refreshDisplay() {
+void setTempUpdate() {
 //  matrix.print(setTemp);
   int setTemp_tens = setTemp/10; 
   int setTemp_ones = setTemp%10;
   matrix.writeDigitNum(0, setTemp_tens);
-  matrix.writeDigitNum(1, setTemp_ones);
+  matrix.writeDigitNum(1, setTemp_ones, false);
+  matrix.writeDigitRaw(3,0);
   matrix.writeDisplay(); // refreshes display with new content
+  delay(1000);
 }
 
+void currentTempUpdate() {
+  currentTemp = get_temperature();
+  int temp = (int)currentTemp;
+  int currentTemp_tens = temp/10; 
+  int currentTemp_ones = temp%10; 
+  int currentTemp_dec = (currentTemp-temp)*10;
+  matrix.writeDigitNum(0, currentTemp_tens); 
+  matrix.writeDigitNum(1, currentTemp_ones, true); 
+  matrix.writeDigitNum(3, currentTemp_dec); 
+  matrix.writeDisplay(); 
+}
+
+float get_temperature() { //Receive temperature measurement
+  sensors.requestTemperatures();
+  return sensors.getTempCByIndex(0);
+}
+void turnOffDisp(){
+    for (int i=0; i<=4; i++){
+      matrix.writeDigitRaw(i,0); 
+    }
+    matrix.writeDisplay();
+}
 
