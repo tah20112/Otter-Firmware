6f26131126d34d477c2b3d1ee4e684d92e9769e7
Celine Ta <celine.ta@students.olin.edu>
2017-08-09 11:06:34 -0400

    restructure to display individual thermistor AND avg readings

--- I2C_LCD_DisplayTemp/I2C_LCD_DisplayTemp.ino
+++ I2C_LCD_DisplayTemp/I2C_LCD_DisplayTemp.ino
@@ -21,7 +21,9 @@ LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars 
 // the value of the 'other' resistor
 #define SERIESRESISTOR 10000    
  
-uint16_t samples[NUMSAMPLES];
+uint16_t sample1[NUMSAMPLES];
+uint16_t sample2[NUMSAMPLES]; 
+uint16_t sample3[NUMSAMPLES];
 uint16_t num_thermistors = 3; 
 
 void setup()
@@ -36,26 +38,66 @@ void setup()
 
 void loop(void) {
   uint8_t i;
-  float average;
+  float average1 = 0.0;
+  float average2 = 0.0;
+  float average3 = 0.0;
+  float avg = 0;
+  float temp1, temp2, temp3, tempAvg;
  
   // take N samples in a row, with a slight delay
-  for (i=0; i< NUMSAMPLES*num_thermistors; i+=num_thermistors) {
-   samples[i] = analogRead(THERMISTORPIN1);
-   samples[i+1]= analogRead(THERMISTORPIN2);
-   samples[i+2]= analogRead(THERMISTORPIN3);
-   delay(10);
+//  for (i=0; i< NUMSAMPLES*num_thermistors; i+=num_thermistors) {
+//   samples[i] = analogRead(THERMISTORPIN1);
+//   samples[i+1]= analogRead(THERMISTORPIN2);
+//   samples[i+2]= analogRead(THERMISTORPIN3);
+//   delay(10);
+//  }
+
+  for (i=0; i< NUMSAMPLES; i++) {
+    sample1[i] = analogRead(THERMISTORPIN1); 
+    sample2[i] = analogRead(THERMISTORPIN2); 
+    sample3[i] = analogRead(THERMISTORPIN3);
+    delay(10);
   }
  
   // average all the samples out
-  average = 0;
-  for (i=0; i< NUMSAMPLES*num_thermistors; i++) {
-     average += samples[i];
+    for (i=0; i< NUMSAMPLES; i++) {
+     average1 += sample1[i];
+     average2 += sample2[i];
+     average3 += sample3[i];
   }
-  average /= NUMSAMPLES*num_thermistors;
+//  average /= NUMSAMPLES*num_thermistors;
+
+  average1 /= NUMSAMPLES; 
+  average2 /= NUMSAMPLES;
+  average3 /= NUMSAMPLES;
+  avg = (average1 + average2 + average3)/num_thermistors;
  
   Serial.print("Average analog reading "); 
-  Serial.println(average);
- 
+  Serial.println(avg);
+  temp1 = getCTemp(average1); 
+  temp2 = getCTemp(average2);
+  temp3 = getCTemp(average3); 
+  tempAvg = getCTemp(avg); 
+  
+  lcd.setCursor(0,0); // set the cursor to column 0, line 0
+  lcd.print("1: ");
+  lcd.print(temp1,1); // print to 1 decimal places
+  lcd.setCursor(8,0); // set the cursor to column 8, line 0
+  lcd.print("2: ");
+  lcd.print(temp2,1); 
+
+  lcd.setCursor(0,1); // set the cursor to column 0, line 1
+  lcd.print("3: ");
+  lcd.print(temp3,1);
+  lcd.setCursor(8,1); // set the cursor to column 8, line 1
+  lcd.print("Avg:");
+  lcd.print(tempAvg,1); 
+  
+  delay(tim); 
+  lcd.clear(); //Clears the LCD screen and positions the cursor in the upper-left corner.
+}
+
+float getCTemp(float average){
   // convert the value to resistance
   average = 1023 / average - 1;
   average = SERIESRESISTOR / average;
@@ -74,12 +116,6 @@ void loop(void) {
   Serial.print(steinhart);
   Serial.println(" *C");
 
-  lcd.setCursor(4,0); // set the cursor to column 15, line 0
-  lcd.print("Avg Temp");
-  lcd.setCursor(5,1); // set the cursor to column 15, line 1
-  lcd.print(steinhart); // Print a message to the LCD.
-  lcd.print(" *C");
-  
-  delay(tim); 
-  lcd.clear(); //Clears the LCD screen and positions the cursor in the upper-left corner.
+  return steinhart;
 }
+
